5d17b009638d56499124a3c2e77f2bd36e649d63
Unknown <edvin_myhren@hotmail.com>
2018-02-06 15:12:30 +0100

    added sorting.go

--- Oblig1/src/algorithms/sorting.go
+++ Oblig1/src/algorithms/sorting.go
@@ -12,10 +12,18 @@ func ExtendedASCIIText() {
 
 // Les https://en.wikipedia.org/wiki/Bubble_sort
 func Bubble_sort_modified(list []int) {
-
-	// Skriv din kode her
-
-
+	end = len(list) -1
+	for {
+		if end == 0 {
+		break
+		}
+	for i := 0; i < len(list) - 1, i++; {
+		if list[i] > list[i+1] {
+		list[i], list[i+1] = list[i+1], list[i]
+		}
+	}
+	end -= 1
+	}
 }
 
 // Implementering av Bubble_sort algoritmen
