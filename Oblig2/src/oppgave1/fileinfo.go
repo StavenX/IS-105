679dce2c0b3785389db29441dd33fe56043246c6
Unknown <edvin_myhren@hotmail.com>
2018-02-27 13:20:35 +0100

    started on info (opg1)

--- Oblig2/src/oppgave1/fileinfo.go
+++ Oblig2/src/oppgave1/fileinfo.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"log"
+)
+
+func info() {
+
+	// Assigns path(?) to file and err
+	file, err := os.OpenFile("Oblig2/src/files/text.txt", os.O_RDWR,0)
+
+	// If there is an error, log error
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch {
+	case err != nil:
+		// handle the error and return
+	case file.Read():
+		// it's a directory
+	default:
+		// it's not a directory
+	}
+
+	defer file.Close()
+}
 
 func main() {
 
 	fmt.Println("Programmet skal returnere informasjon om en fil")
-
+	info()
 }
