ff4907759eb0d042ccf9ccc7af0b9319aa67d094
Unknown <endre.b.g@gmail.com>
2018-03-05 17:38:59 +0100

    Did opg2
    i did it

--- Oblig2/src/oppgave2/filecount.go
+++ Oblig2/src/oppgave2/filecount.go
@@ -1,7 +1,72 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"strings"
+	)
+
+var RuneMap = make(map[rune]int)
 
 func main() {
-	fmt.Println("Programmet skal lese en tekst-fil, skrive ut totalt antall linjer og antall for fem runes som forekommer hyppigst i filen")
+	//Count the lines
+	args := os.Args
+	if len(args) == 0 {
+		fmt.Println("Missing argument (File)")
+		fmt.Println("Usage: go run fileinfo.go myfile.txt")
+		return
+	}
+
+	filename := args[1]
+
+	file, _ := os.Open(filename)
+	fileScanner := bufio.NewScanner(file)
+	lineCount := 0
+	for fileScanner.Scan() {
+		lineCount++
+	}
+	fmt.Println(lineCount)
+
+	//Converts file to string
+	f, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Print(err)
+	}
+	str := string(f)
+
+	//Adressing the individual runes in the string
+	splitString := []string(strings.Split(str, ""))
+
+
+	for i := 0; i < len(splitString); i++ {
+		MapHandler(splitString[i])
+	}
+	fmt.Println(len(RuneMap))
+	fmt.Println(RuneMap)
+}
+var array []string
+
+func MapHandler(char string) {
+	foundIt := false
+	/*for i := 0; i < len(RuneMap); i++ {
+		if char == string(RuneMap[rune(i)]) {
+			foundIt = true
+			fmt.Println("yay")
+		}
+	}*/
+	for i := 0; i < len (array); i++ {
+		if char == array[i] {
+			foundIt = true
+		}
+	}
+	char2 := []rune(char)
+	char3 := int(char2[0])
+	if foundIt == false {
+		RuneMap[rune(char3)] = 1
+		array = append(array, char)
+	} else {
+		RuneMap[rune(char3)]++
+	}
 }
