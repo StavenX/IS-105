fb3e808e44a3cee8736fc4de2e230f46d912b6b0
Unknown <edvin_myhren@hotmail.com>
2018-04-09 15:43:55 +0200

    - added explaining comments - removed irrelevant code

--- Oblig3/src/oppgave1/http1.go
+++ Oblig3/src/oppgave1/http1.go
@@ -5,16 +5,14 @@ import (
 	"net/http"
 )
 
+// Simple method that greets when accessing the server
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello client.")
 }
 
-func test(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello test .")
-}
-
 func main() {
+	// If path that is accessed is "/", server will respond with 'hello' method
 	http.HandleFunc("/", hello)
-	http.HandleFunc("/1", test)
+	// Server listens on port 8080
 	http.ListenAndServe(":8080", nil)
 }
