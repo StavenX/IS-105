57a18b14194d907d00564642d218875fcaeb6e0c
Unknown <edvin_myhren@hotmail.com>
2018-04-18 15:08:36 +0200

    started working on new test function

--- Oblig3/src/oppgave2/test/test.go
+++ Oblig3/src/oppgave2/test/test.go
@@ -7,77 +7,49 @@ import (
 	"encoding/json"
 	//"strconv"
 	"strconv"
+	"net/http"
 )
 
-type Entries struct {
-	Entries []Datasett `json:"datasets"`
-}
 
-type Datasett struct {
-	ID string `json:"id"`
-	Title string `json:"title"`
-	Description string `json:"description"`
-	LandingPage string `json:"landingPage"`
-	Issued string `json:"issued"`
-	Modified string `json:"modified"`
-	Language string `json:"language"`
-	Publisher string `json:"publisher"`
-	Name string `json:"name"`
-	MailBox string `json:"mbox"`
-	Keyword string `json:"keyword"`
-}
-
-type Distribusjon struct {
-	Title string `json.datasets:"distribusjon"`
-}
 
 func main() {
-// Open our jsonFile
-jsonFile, err := os.Open("test.json")
-
-// if we os.Open returns an error then handle it
-if err != nil {
-	fmt.Println(err)
+	openJson()
+	http.HandleFunc("/1", printToServer())
+	http.ListenAndServe(":8080", nil)
 }
-fmt.Println("Successfully Opened test.json")
-fmt.Println("https://data.norge.no/api/dcat/data.json?page=1")
 
-// defer the closing of our jsonFile so that we can parse it later on
-defer jsonFile.Close()
+// ---------------------------------------------------
 
-// read our opened xmlFile as a byte array.
-byteValue, _ := ioutil.ReadAll(jsonFile)
+type Datasets struct {
+	Datasets []Dataset `json:"datasets"`
+}
 
+type Dataset struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Antall string `json:"antall"`
+	Description []Description `json:"description"`
+	Distribution []Distribution `json:"distribution"`
+}
 
-// we initialize our Users array
-var entries Entries
+type Distribution struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+}
 
+type Description struct {
+	Language string `json:"language"`
+	Value string `json:"value"`
+}
 
-// we unmarshal our byteArray which contains our
-// jsonFile's content into 'users' which we defined above
-json.Unmarshal(byteValue, &entries)
+var datasets Datasets 
 
+// -------------------------------------------------
 
-// we iterate through every user within our users array and
-// print out the user Type, their name, and their facebook url
+func openJson() {
 
-for i := 0; i < len(entries.Entries); i++ {
-fmt.Println(" ")
-fmt.Println("Entry: " + strconv.Itoa(i))
-fmt.Println("ID: " + entries.Entries[i].ID)
-fmt.Println("Title: " + entries.Entries[i].Title)
-fmt.Println("Description: " + entries.Entries[i].Description)
-fmt.Println("Landing page: " + entries.Entries[i].LandingPage)
-fmt.Println("Issued: " + entries.Entries[i].Issued)
-fmt.Println("Modified: " + entries.Entries[i].Modified)
-fmt.Println("Language: " + entries.Entries[i].Language)
-fmt.Println("Publisher: " + entries.Entries[i].Publisher)
-fmt.Println("Name: " + entries.Entries[i].Name)
-fmt.Println("MailBox: " + entries.Entries[i].MailBox)
-fmt.Println("Keyword: " + entries.Entries[i].Keyword)
-//fmt.Println("Distribusjon: " + entries.Entries[i].Distribusjon)
+}
 
-//fmt.Println(" ")
+func printToServer() {
 
-
-}}
+}
