9302ab33f4100982c714eacbba63777e5be9464c
Unknown <edvin_myhren@hotmail.com>
2018-04-30 15:14:12 +0200

    Updated structs
    - added proper struct for Earthquake

--- Oblig4/test.go
+++ Oblig4/test.go
@@ -15,6 +15,38 @@ type Earthquakes struct {
 	Earthquakes []Earthquake `json:"features"`
 }
 
+type Earthquake struct {
+	Type string `json:"type"`
+	Properties struct {
+
+		Magnitude float32 	`json:"mag"`
+		Place string 		`json:"place"`
+		Time int64 			`json:"time"`
+		Updated int64 		`json:"time"`
+		TimeZone int32 		`json:"tz"`
+		URL string 			`json:"url"`
+		Detail string 		`json:"detail"`
+		Felt int32 			`json:"felt"`
+		CDI float32 		`json:"cdi"`
+		MMI float32 		`json:"mmi"`
+		Alert string 		`json:"alert"`
+		Status string 		`json:"status"`
+		Tsunami int32 		`json:"tsunami"`
+		SIG int32 			`json:"sig"`
+		NET string 			`json:"net"`
+		Code string 		`json:"code"`
+		IDS string 			`json:"ids"`
+		Sources string 		`json:"sources"`
+		Types string 		`json:"types"`
+		NST int32 			`json:"nst"`
+		DMIN float32 		`json:"dmin"`
+		RMS float32 		`json:"rms"`
+		GAP float32 		`json:"gap"`
+		MagType string 		`json:"magType"`
+		Type string 		`json:"type"`
+	} `json:"properties"`
+}
+
 type Header struct {
 	Type     string `json:"type"`
 	Metadata struct {
@@ -28,6 +60,7 @@ type Header struct {
 }
 
 var entries Earthquakes
+var header Header
 
 // ----------------------------
 
@@ -66,21 +99,38 @@ func getJson() {
 
 	fmt.Printf("Length: %d\n", len(entries.Earthquakes))
 
-	for i := 0; i < len(entries.Earthquakes); i++ {
-		PrintAll(i)
-		fmt.Println(i)
-	}
+		PrintEarthquakesToConsole()
 }
 
-func PrintAll(index int) {
-	d:= entries.Earthquakes[index]
-	fmt.Println()
-	fmt.Println("Printing METADATA: ")
-	fmt.Println("Type: ", d.Type)
-	fmt.Println("Generated: ", d.Metadata.Generated)
-	fmt.Println("URL: ", d.Metadata.URL)
-	fmt.Println("Title: ", d.Metadata.Title)
-	fmt.Println("API: ", d.Metadata.API)
-	fmt.Println("Count: ", d.Metadata.Count)
-	fmt.Println("Status: ", d.Metadata.Status)
+func PrintEarthquakesToConsole() {
+	for i := 0; i < len(entries.Earthquakes); i++ {
+		d:= entries.Earthquakes[i]
+		fmt.Println()
+		fmt.Println("Magnitude: ", 	d.Properties.Magnitude)
+		fmt.Println("Place: ", 		d.Properties.Place)
+		fmt.Println("Time: ", 		d.Properties.Time)
+		fmt.Println("Updated: ", 	d.Properties.Updated)
+		fmt.Println("TimeZone: ", 	d.Properties.TimeZone)
+		fmt.Println("Detail: ", 		d.Properties.Detail)
+		fmt.Println("Felt: ", 		d.Properties.Felt)
+		fmt.Println("CDI: ", 		d.Properties.CDI)
+		fmt.Println("MMI: ", 		d.Properties.MMI)
+		fmt.Println("Alert: ", 		d.Properties.Alert)
+		fmt.Println("Status: ", 		d.Properties.Status)
+		fmt.Println("Tsunami: ", 	d.Properties.Tsunami)
+		fmt.Println("Significance: ",d.Properties.SIG)
+		fmt.Println("NET: ", 		d.Properties.NET)
+		fmt.Println("Code: ", 		d.Properties.Code)
+		fmt.Println("IDS: ", 		d.Properties.IDS)
+		fmt.Println("Sources: ", 	d.Properties.Sources)
+		fmt.Println("Types: ", 		d.Properties.Types)
+		fmt.Println("NST: ", 		d.Properties.NST)
+		fmt.Println("DMIN: ", 		d.Properties.DMIN)
+		fmt.Println("RMS: ", 		d.Properties.RMS)
+		fmt.Println("GAP: ", 		d.Properties.GAP)
+		fmt.Println("MagType: ", 	d.Properties.MagType)
+		fmt.Println("Type: ", 		d.Properties.Type)
+
+	}
+
 }
