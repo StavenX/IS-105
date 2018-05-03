9043d755b450eed6b2b21a3eaaa9a9248141dec0
Unknown <edvin_myhren@hotmail.com>
2018-05-03 13:35:53 +0200

    Reduced code duplication
    - made the printing of an earthquake's individual data it's own function, that can be called in other specific url functions

--- Oblig4/test.go
+++ Oblig4/test.go
@@ -68,6 +68,87 @@ type Header struct {
 
 }
 
+var entries Earthquakes
+var header Header
+
+// ----------------------------
+
+func main() {
+
+ 	openServer()
+	//getJson("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
+}
+
+// Uses an URL as parameter for the getJson. Works only on URL's from
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+func getJson(_url string) {
+
+	url := _url
+
+	client := http.Client{
+		Timeout: time.Second *2,
+	}
+
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Println("Request error.")
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println("Response error.")
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Body error.")
+	}
+
+	jsonerr := json.Unmarshal(body, &entries)
+	if jsonerr != nil {
+		fmt.Println("JSON entries unmarshal error.")
+	}
+
+	header_jsonerr := json.Unmarshal(body, &header)
+	if header_jsonerr != nil {
+		fmt.Println("JSON header unmarshal error.")
+	}
+
+	//fmt.Printf("Amount of earthquakes: %d\n", len(entries.Earthquakes))
+
+	// Prints for earthquake information
+	PrintHeaderToConsole()
+	PrintEarthquakesToConsole()
+}
+
+// Opens a server on the given port. Will greet the user on a default path
+func openServer() {
+	// Handles every "/" request. Also works on other "/"
+	// that are not handled.
+	http.HandleFunc("/", printHello)
+
+	// Handler for the individual pages we have selected.
+	http.HandleFunc("/1", PrintEarthquakesToServer1)
+	http.HandleFunc("/header", PrintHeaderToServer)
+
+	// Opens the server on the given port
+	http.ListenAndServe(":8080", nil)
+}
+
+// Prints hello to the client
+func printHello(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(writer, "Hello client")
+}
+// Takes Unix time as parameter, and returns time in a readable format
+func getUnixAsReadable(_time int64) time.Time  {
+	t := time.Unix((
+		int64(_time) / 1000), // +
+		//int64(_timeZone) * 60,
+		0)
+
+	return t
+}
+
 // Prints the JSON header to the server (webpage)
 func PrintHeaderToServer(writer http.ResponseWriter, request *http.Request) {
 	getJson("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
@@ -121,108 +202,23 @@ func PrintHeaderToConsole() {
 	*/
 }
 
-var entries Earthquakes
-var header Header
-
-// ----------------------------
-
-func main() {
-
- 	openServer()
-
-	//getJson("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
-}
-
-// Uses an URL as parameter for the getJson. Works only on URL's from
-// https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
-func getJson(_url string) {
-
-	url := _url
-
-	client := http.Client{
-		Timeout: time.Second *2,
-	}
-
-	request, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		fmt.Println("Request error.")
-	}
-
-	response, err := client.Do(request)
-	if err != nil {
-		fmt.Println("Response error.")
-	}
-
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println("Body error.")
-	}
-
-	jsonerr := json.Unmarshal(body, &entries)
-	if jsonerr != nil {
-		fmt.Println("JSON entries unmarshal error.")
-	}
-
-	header_jsonerr := json.Unmarshal(body, &header)
-	if header_jsonerr != nil {
-		fmt.Println("JSON header unmarshal error.")
-	}
-
-	//fmt.Printf("Amount of earthquakes: %d\n", len(entries.Earthquakes))
-
-	// Prints for earthquake information
-	PrintHeaderToConsole()
-	PrintEarthquakesToConsole()
-}
-
-func openServer() {
-	// Handles every "/" request. Also works on other "/"
-	// that are not handled.
-	http.HandleFunc("/", printHello)
-
-	// Handler for the individual pages we have selected.
-	http.HandleFunc("/1", PrintEarthquakesToServer1)
-	http.HandleFunc("/2", PrintEarthquakesToServer2)
-	http.HandleFunc("/header", PrintHeaderToServer)
-
-	// Opens the server on the given port
-	http.ListenAndServe(":8080", nil)
-
-}
-
-// Takes Unix time as parameter, and returns time in a readable format
-func getUnixAsReadable(_time int64) time.Time  {
-	t := time.Unix((
-		int64(_time) / 1000), // +
-		//int64(_timeZone) * 60,
-		0)
-
-	return t
-}
-
-// Prints hello to the client
-func printHello(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintln(writer, "Hello client")
-}
-
-
+// Print earthquake data to the server console
 func PrintEarthquakesToServer1(writer http.ResponseWriter, request *http.Request,) {
 	getJson("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
-
 	PrintHeaderToServer(writer,request)
 	fmt.Fprintln(writer)
 	fmt.Fprintln(writer, "List of earthquakes: ")
+	PrintEarthquakeInformation(writer)
+}
 
+// Function to print individual information about an earthquake. Reduces code duplucation, and prevents
+// the need for writing many, long functions with the same data
+func PrintEarthquakeInformation(writer http.ResponseWriter) {
 	for i := 0; i < len(entries.Earthquakes); i++ {
 		d:= entries.Earthquakes[i]
 		fmt.Fprintln(writer)
 		fmt.Fprintln(writer,"Place: ", 		d.Properties.Place)
 		fmt.Fprintln(writer,"Magnitude: ", 	d.Properties.Magnitude)
-
-		// Converting time in Unix format to readable time
-		//t := time.Unix(int64(d.Properties.Time) / 1000, 0)
-		//fmt.Fprintln(writer, "Time: ", t)
-
 		fmt.Fprintln(writer, "Time: ", 		getUnixAsReadable(d.Properties.Time))
 		fmt.Fprintln(writer,"Updated: ", 	getUnixAsReadable(d.Properties.Updated))
 		fmt.Fprintln(writer,"TimeZone: ", 	d.Properties.TimeZone)
@@ -247,42 +243,8 @@ func PrintEarthquakesToServer1(writer http.ResponseWriter, request *http.Request
 		fmt.Fprintln(writer,"Type: ", 		d.Properties.Type)
 	}
 }
-func PrintEarthquakesToServer2(writer http.ResponseWriter, request *http.Request,) {
-	getJson("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
-	fmt.Fprintln(writer)
-	fmt.Fprintln(writer, "List of earthquakes: ")
 
-	for i := 0; i < len(entries.Earthquakes); i++ {
-		d:= entries.Earthquakes[i]
-		fmt.Fprintln(writer)
-		fmt.Fprintln(writer,"Magnitude: ", 	d.Properties.Magnitude)
-		fmt.Fprintln(writer,"Place: ", 		d.Properties.Place)
-		fmt.Fprintln(writer,"Time: ", 		d.Properties.Time)
-		fmt.Fprintln(writer,"Updated: ", 	d.Properties.Updated)
-		fmt.Fprintln(writer,"TimeZone: ", 	d.Properties.TimeZone)
-		fmt.Fprintln(writer,"Detail: ", 		d.Properties.Detail)
-		fmt.Fprintln(writer,"Felt: ", 		d.Properties.Felt)
-		fmt.Fprintln(writer,"CDI: ", 		d.Properties.CDI)
-		fmt.Fprintln(writer,"MMI: ", 		d.Properties.MMI)
-		fmt.Fprintln(writer,"Alert: ", 		d.Properties.Alert)
-		fmt.Fprintln(writer,"Status: ", 		d.Properties.Status)
-		fmt.Fprintln(writer,"Tsunami: ", 	d.Properties.Tsunami)
-		fmt.Fprintln(writer,"Significance: ",d.Properties.SIG)
-		fmt.Fprintln(writer,"NET: ", 		d.Properties.NET)
-		fmt.Fprintln(writer,"Code: ", 		d.Properties.Code)
-		fmt.Fprintln(writer,"IDS: ", 		d.Properties.IDS)
-		fmt.Fprintln(writer,"Sources: ", 	d.Properties.Sources)
-		fmt.Fprintln(writer,"Types: ", 		d.Properties.Types)
-		fmt.Fprintln(writer,"NST: ", 		d.Properties.NST)
-		fmt.Fprintln(writer,"DMIN: ", 		d.Properties.DMIN)
-		fmt.Fprintln(writer,"RMS: ", 		d.Properties.RMS)
-		fmt.Fprintln(writer,"GAP: ", 		d.Properties.GAP)
-		fmt.Fprintln(writer,"MagType: ", 	d.Properties.MagType)
-		fmt.Fprintln(writer,"Type: ", 		d.Properties.Type)
-	}
-}
-
-
+// Prints earthquake data to local console
 func PrintEarthquakesToConsole() {
 
 	fmt.Println()
