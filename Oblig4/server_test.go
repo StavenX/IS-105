3595160b62a6b622ae03f8c67ee28653660f346a
Unknown <endre.b.g@gmail.com>
2018-05-11 14:48:14 +0200

    removed all month from the test

--- Oblig4/server_test.go
+++ Oblig4/server_test.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"testing"
+	"time"
 )
 
 //Checks if the url holds a json
 func TestGetJson(t *testing.T) {
-	array := []string{"all_hour.geojson","all_week.geojson","all_day.geojson","all_month.geojson"}
-	for i:=0 ; i < 4; i++ {
+	array := []string{"all_hour.geojson","all_week.geojson","all_day.geojson"}
+	for i:=0 ; i < 3; i++ {
 		server := GetJson("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/"+array[i])
+		time.Sleep(time.Second*5)
 		if server != nil {
 			t.Errorf("There is no Json in " + array[i])
 		}
